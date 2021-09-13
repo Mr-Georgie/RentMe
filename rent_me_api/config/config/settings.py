fa004082681937175a44dd63274fa9f051683f1c
mr-georgie <george.isiguzo@yahoo.com>
2021-09-13 18:07:37 +0100

    added custom error handler

--- rent_me_api/config/config/settings.py
+++ rent_me_api/config/config/settings.py
@@ -188,12 +188,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_SSL = True    # use port 465
-EMAIL_USE_TLS = False    # use port 587
-EMAIL_PORT = 465 # OR 587
+# EMAIL_USE_SSL = True    # use port 465
+EMAIL_USE_SSL = False    # use port 587
+# EMAIL_PORT = 465 # OR 587
 
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
