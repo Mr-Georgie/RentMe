b7dc725a10e24159cb8aed8a46f7ba665ace55a5
George Isiguzo <george.isiguzo@yahoo.com>
2021-09-17 22:18:39 +0100

    Edited

--- rent_me_api/config/user_dashboard/permissions.py
+++ rent_me_api/config/user_dashboard/permissions.py
@@ -4,4 +4,4 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
     
     def has_object_permission(self, request, view, obj):
-        return obj.created_by == request.user
+        return obj.owner == request.user
