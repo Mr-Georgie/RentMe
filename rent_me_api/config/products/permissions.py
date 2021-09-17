e78dda7e47f1cca06c1fc1b197cb8a4169355e3d
George Isiguzo <george.isiguzo@yahoo.com>
2021-09-17 18:29:41 +0100

    Delete rent_me_api/config/products directory

--- rent_me_api/config/products/permissions.py
+++
@@ -1,7 +0,0 @@
-# custom file
-from rest_framework import permissions
-
-class IsOwner(permissions.BasePermission):
-    
-    def has_object_permission(self, request, view, obj):
-        return obj.created_by == request.user
