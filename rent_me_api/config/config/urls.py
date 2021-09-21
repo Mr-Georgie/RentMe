7cda8481db120e880e79787c97ac3c6edc39d785
mr-georgie <george.isiguzo@yahoo.com>
2021-09-21 07:08:13 +0100

    update from locals

--- rent_me_api/config/config/urls.py
+++ rent_me_api/config/config/urls.py
@@ -54,7 +54,9 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'utils.views.error_404'
 handler500 = 'utils.views.error_500'
